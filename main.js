5f59c25aefbd4d64b2c0cbd7a55a38df654cc63e
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-07-07 17:17:37 +0100

    added fucntion to check off to do

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const addButton = document.getElementById('add-button');
 const toDoListContainer = document.getElementById('to-do-list-container')
 
 const addToDo = () => {
-    toDoListContainer.innerHTML += `<li class="to-do"><p>${toDoInput.value}</p> <p class="delete">x</p></li>`;
+    toDoListContainer.innerHTML += `<li class="to-do">${toDoInput.value} <p class="delete">x</p></li>`;
     toDoInput.value = '';
     saveToDo()
 }
@@ -16,6 +16,12 @@ toDoListContainer.addEventListener('click', (e) => {
         }
         saveToDo()
     });
+
+toDoListContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('to-do')) {
+        e.target.classList.toggle('done')
+    }
+})
 
 saveToDo = () => {
    localStorage.setItem('toDoData', toDoListContainer.innerHTML) 
